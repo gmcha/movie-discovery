67c5c72bf6841ed0c70d88c2952268222f71a292
Ga Min Cha <prettygamin@naver.com>
2023-01-11 10:59:34 +0900

    Update: #7.2 challenge - coin conversion

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -3,6 +3,21 @@ import {useState, useEffect} from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [USD, setUSD] = useState(0);
+  const [conversion, setConversion] = useState(0);
+  const [cost, setCost] = useState(0);
+  const onSelected = (event) => {
+    console.dir(event);    
+    setCost(coins[event.target.selectedIndex].quotes.USD.price);
+    console.log(cost);
+  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setConversion(USD/cost);
+  }
+  const onChange = (event) => {
+    setUSD(event.target.value);
+  };
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
     .then((response) => response.json())
@@ -10,16 +25,22 @@ function App() {
       setCoins(json); 
       setLoading(false);
     });
-
   }, [])
   return (
   <div>
     <h1>The Coins!</h1>
     {loading ? <strong>Loading...</strong> : (
-      <select>
-      {coins.map((coin) => (<option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>))}
+      <select onChange={onSelected}>
+      {coins.map((coin) => (<option value={cost}>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>))}
       </select>
     )}
+    <hr/>
+    <form onSubmit={onSubmit}>
+      <input onChange={onChange} value={USD} type="number" placeholder="$USD"/>
+      <button>Convert</button>
+      <br />
+      <input value={conversion} type="number" placeholder="Coin Conversion"/>
+    </form>
   </div>
   );
 } 
