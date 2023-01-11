ee14cb33e0a88d3d171bbb1fa695f6bce5cd6ecc
Ga Min Cha <prettygamin@naver.com>
2023-01-11 09:12:48 +0900

    Update: #7.0

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,22 +1,27 @@
-import {useState, useEffect} from "react";
-
-function Hello() {
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
+import {useState} from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
-  return(
-    <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-    </div>
-  ); 
-}
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value); 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo == ""){
+      return;
+    } 
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  }
+  console.log(toDos);
+  return (
+  <div>
+    <h1>My To Dos ({toDos.length})</h1>
+    <form onSubmit={onSubmit}>
+      <input onChange = {onChange} value={toDo} type="text" placeholder="Write your todo..." />
+      <button>Add To Do</button>
+    </form>
+  </div>
+  );
+} 
 
 export default App;
