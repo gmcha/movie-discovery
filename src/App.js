0ccbd4feb6120cff7bbb7e6df67f0ff2a9f41963
Ga Min Cha <prettygamin@naver.com>
2023-01-12 12:39:14 +0900

    Update: App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/movie" element={<Detail />} />
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 } 
 
